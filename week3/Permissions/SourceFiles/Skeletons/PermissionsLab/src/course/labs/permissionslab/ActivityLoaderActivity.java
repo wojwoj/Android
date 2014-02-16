7c2f5280fa23cfec29768e42da39dd6bd6955ee0
Wojciech Wojcik <wojciech.wojcik@seamless.se>
2014-02-16 14:37:22 +0100

    adding ex 1 ex2 for Permissions

--- week3/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/ActivityLoaderActivity.java
+++ week3/Permissions/SourceFiles/Skeletons/PermissionsLab/src/course/labs/permissionslab/ActivityLoaderActivity.java
@@ -33,6 +33,9 @@ public class ActivityLoaderActivity extends Activity {
 		Log.i(TAG, "Entered startBookMarksActivity()");
 	
 		// TODO - Start the BookmarksActivity
+		
+		Intent intent = new Intent(getApplicationContext(),BookmarksActivity.class);
+		startActivity(intent);
 
 	}
 }
